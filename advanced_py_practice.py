b962d1e4e4f32d2c56a5d75aee833334c31afd7a
iamdanishasim <iamdanishasim@gmail.com>
2026-06-01 22:52:13 +0500

    Add files via upload
    Decorators and Args & Kwargs

---
+++ advanced_py_practice.py
@@ -0,0 +1,37 @@
+# Practicing args and kwargs 
+def Decorate(func):
+    def wrapper(num):
+        print("Welcome Stranger")
+        func(num)
+        print("Thanks for Using our Services")
+    return wrapper  
+
+@Decorate
+def evenOrNot(a):
+    if a % 2 == 0:
+        print("The Number is Even")
+    else:
+        print("The Number is Odd")
+
+evenOrNot(2)
+
+
+# Practicing List comprehension 
+list1 = [i for i in range(21) if i % 2 == 0 ]
+print(list1)
+
+
+#Lambda 
+answer = lambda num : "Even" if num % 2 == 0 else "pdd"
+num_input = int(input("Please Enter a Number ")) 
+print(answer(num_input))
+
+#map & Filter 
+list2 = [1,2,3,4,5]
+
+result = map(lambda x : x**2 ,list2)
+print(list(result))
+
+secondResult = filter(lambda x : True if x%2 == 0 else False, list2)
+
+print(list(secondResult))
